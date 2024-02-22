8d4ef9288f9d8e087c0cba2d1fbacea5595a5799
TomasGnG <tomaskeder2005@gmail.com>
2024-02-22 22:41:46 +0100

    Initial commit

---
+++ src/main/java/de/tomasgng/multitimer/MultiTimer.java
@@ -0,0 +1,17 @@
+package de.tomasgng.multitimer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MultiTimer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
